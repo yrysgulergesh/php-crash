1ba9af1acfa5365c4c4e1b752bafb76d3819d508
Yrysgul Ergesh <yrysgulergesh@gmail.com>
2022-09-22 22:15:46 +0600

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$userName; //Camel case
+$username; //Snake case
+
+$name = 'Jack';
+$century=20;
+echo $name;
+echo 'Hello World! <br>';
+echo 'Hello $name! <br>';
+echo 'Hello name! <br>';
+
+
+
+?>
