13f5f1015d9f55d8afb093f4af7650059cadcc4f
Yrysgul Ergesh <yrysgulergesh@gmail.com>
2022-11-08 03:14:32 +0600

    func php echo

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php
+// echo - Output strings, numbers, html, etc
+// echo 123, 'Hello', 10,5
 
-$userName; //Camel case
-$username; //Snake case
+// print - Works like echo, but can only take in a single argument
+//print 123;
 
-$name = 'Jack';
-$century=20;
-echo $name;
-echo 'Hello World! <br>';
-echo 'Hello $name! <br>';
-echo 'Hello name! <br>';
+// print_r() - Print single values and arrays
+// print_r([1,2,3,]);
 
 
+// var_dump() - Returns more info like data type and length
+//var_dump(true);
 
+// var_export() - Similar to var_dump(). Outputs a string representation of a variable
+// var_export('Hello')
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'Post One'; ?></h1>
+</body>
+</html>
