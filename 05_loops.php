8d15219b7dec578727000bcceeb4b9b7d44c52be
Yrysgul Ergesh <yrysgulergesh@gmail.com>
2022-11-09 22:50:54 +0600

    loops

--- 05_loops.php
+++ 05_loops.php
@@ -1,1 +1,83 @@
 <?php
+/* ------------- For Loop ------------- */
+
+/*
+ ** For Loop Syntax
+  for (initialize; condition; increment) {
+    // code to be executed
+    }
+ */
+
+
+for ($x = 0; $x <= 10; $x++) {
+  echo 'Number ' . $x . '<br>';
+}
+
+/* ------------- While Loop ------------ */
+
+/*
+ ** While Loop Syntax
+   while (condition) {
+    // code to be executed
+}
+ */
+
+$x = 1;
+
+while($x <= 15) {
+    echo 'Number ' . $x . '<br>';
+    $x = $x + 1;
+}
+
+/* ------------- Do While Loop ------------ */
+
+/*
+ ** Do While Loop Syntax
+   do {
+    // code to be executed
+    } while (condition);
+
+do...while loop will always execute the block of code once,
+even if the condition is false.
+ */
+
+//
+$x = 6;
+
+do {
+    echo 'Number ' . $x . '<br>';
+    $x++;
+} while($x <= 5);
+
+/* ------------- Foreach Loop ------------- */
+
+/*
+ ** Foreach Loop Syntax
+  foreach ($array as $value) {
+    // code to be executed
+    }
+ */
+
+$posts = ['First Post', 'Second Post', 'Third Post'];
+
+for($x = 0; $x < count($posts);$x++) {
+    echo $posts[$x];
+}
+
+foreach($posts as $post) {
+    echo $post;
+}
+
+foreach ($posts as $index => $post) {
+    echo $index + 1 . '_' . $post . '<br>';
+}
+
+$person = [
+    'first_name' => 'Brad',
+    'last_name' => 'Traversy',
+    'email' => 'brad@gmail.com',
+];
+
+foreach ($person as $key => $value) {
+    echo "$key - $value<br>";
+}
